a43647ba74bafe3fb50c57d56646b8488cd51bea
Kevin Clark <naeaman@gmail.com>
2019-12-13 10:05:07 -0700

    pushing react calculator

---
+++ src/components/ClearButton.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './clearbutton.css';
+
+class ClearButton extends Component {
+    render() {
+        return(
+            <div className='clear-btn'
+            onClick={() => this.props.handleClear()} >
+                {this.props.children}
+                Clear
+            </div>
+        )
+    }
+}
+
+export default ClearButton
